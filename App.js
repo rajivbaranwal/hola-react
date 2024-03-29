5ba0baa146de447ad4a45a1d76b9c493807fbc39
rajivbaranwal <rajivbar04@gmail.com>
2024-03-29 12:53:26 +0530

    ep-1.1

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
+/**
+ * <div id="parent">
+ * <div id="child">
+ * <h1></h1>
+ * </div>
+ * </div>
+ *
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "h1" }, "i am h1 tag"),
+    React.createElement("h2", { id: "h2" }, "i am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "h1" }, "i am h1 tag"),
+    React.createElement("h2", { id: "h2" }, "i am h2 tag"),
+  ])
+);
+
 const heading = React.createElement(
   "h1",
   { id: "heading", xyx: "abc" },
   "i am h1 tag"
 );
-console.log(heading);
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
